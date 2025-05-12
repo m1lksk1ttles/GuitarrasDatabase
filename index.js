6acdc66d8c633bc4216ee5a07d7baedbde07b1dc
Juan Jaime Gutierrez <m1lksk1ttles@outlook.com>
2025-05-12 01:52:20 -0600

    ewqwqeqwewqe

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Configura tu conexión a MySQL
 const connection = mysql.createConnection({
-  host: ' yamabiko.proxy.rlwy.net:22279',
-  user: 'root',
-  password: '', // O pon la contraseña correcta si tiene
-  database: 'c22100440',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-// Conectar a la base de datos
 connection.connect(err => {
   if (err) {
     console.error('Error al conectar a MySQL:', err);
@@ -21,7 +20,6 @@ connection.connect(err => {
   console.log('Conectado a MySQL');
 });
 
-// Ruta HTML que muestra datos de una tabla
 app.get('/', (req, res) => {
   connection.query('SELECT * FROM guitarras', (err, results) => {
     if (err) {
