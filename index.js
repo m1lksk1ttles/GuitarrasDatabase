2c3cc83f051de60a8e8c42fbab40eda9e100d4b0
Juan Jaime Gutierrez <m1lksk1ttles@outlook.com>
2025-05-12 00:53:09 -0600

    process.env.port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Configura tu conexión a MySQL
 const connection = mysql.createConnection({
