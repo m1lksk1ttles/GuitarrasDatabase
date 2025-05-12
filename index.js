c7f217538cab48c61b63e7c59ca57d9234ee714b
Juan Jaime Gutierrez <m1lksk1ttles@outlook.com>
2025-05-12 01:27:27 -0600

    ???

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 5000;
 
 // Configura tu conexión a MySQL
 const connection = mysql.createConnection({
-  host: 'mysql://root:lCvVOMLVAVKSadzzYnsoWyZIZfPXRYXl@yamabiko.proxy.rlwy.net:38807/railway',
+  host: '187.254.18.202',
   user: 'root',
   password: '', // O pon la contraseña correcta si tiene
-  database: 'c22100440',
+  database: 'lCvVOMLVAVKSadzzYnsoWyZIZfPXRYXl',
   port: 3306
 });
 
